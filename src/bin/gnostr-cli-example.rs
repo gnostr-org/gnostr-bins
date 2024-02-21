818565589854c22101377ba4be68f6559c025185
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-02-21 11:19:46 -0500

    src/bin/gnostr-cli-example.rs:self reference reqwest

--- src/bin/gnostr-cli-example.rs
+++ src/bin/gnostr-cli-example.rs
@@ -1,7 +1,7 @@
 use std::io::{Read};
 use reqwest::Url;
 fn main() {
-    let url = Url::parse("https://www.rust-lang.org/").unwrap();
+    let url = Url::parse("https://raw.githubusercontent.com/gnostr-org/gnostr-bins/master/src/bin/gnostr-cli-example.rs").unwrap();
     let mut res = reqwest::blocking::get(url).unwrap();
 
     let mut body = String::new();
