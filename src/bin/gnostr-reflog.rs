9a6c1959620abca00bfa497cc2d9f0eb135831c3
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-02-23 22:02:36 -0500

    src/bin/gnostr-reflog.rs:intermediate

--- src/bin/gnostr-reflog.rs
+++ src/bin/gnostr-reflog.rs
@@ -1,33 +1,13 @@
 extern crate getopts;
 use getopts::Options;
-use std::env;
-//use git2::{Repository, Revwalk, Commit};
 use git2::{Commit, Repository};
-use std::io;
-use ascii::AsciiChar;
+
+use std::env;
 use std::process;
 
-fn print_usage(program: &str, opts: &Options) {
-    let brief = format!("Usage: {} [options]", program);
-    print!("{}", opts.usage(&brief));
-    process::exit(0);
-}
-fn sec(program: &str, opts: &Options) {
-    let brief = format!("Usage: {} [options]", program);
-    print!("sec:\n{}", opts.usage(&brief));
-    process::exit(0);
-}
-fn hash(program: &str, opts: &Options) {
-    //hash a following value
-    //TODO detect stream or file
-    let brief = format!("Usage: {} [options]", program);
-    print!("hash:\n{}", opts.usage(&brief));
-    process::exit(0);
-}
-
-pub fn hash_list(_program: &str, _opts: &Options) -> Result<(), git2::Error> {
+pub fn ref_hash_list(_program: &str, _opts: &Options) -> Result<(), git2::Error> {
     //let brief = format!("Usage: {} FILE [options]", _program);
-    //print!("hash_list_commit_message:\n{}", _opts.usage(&brief));
+    //print!("ref_hash_list_commit_message:\n{}", _opts.usage(&brief));
     let repo = match Repository::open(".") {
         Ok(repo) => repo,
         Err(e) => panic!("Error opening repository: {}", e),
@@ -44,10 +24,11 @@ pub fn hash_list(_program: &str, _opts: &Options) -> Result<(), git2::Error> {
     }
     Ok(())
     // process::exit(0);
-}
-pub fn hash_list_w_commit_message(_program: &str, _opts: &Options) -> Result<(), git2::Error> {
+} //end ref_hash_list
+
+pub fn ref_hash_list_w_commit_message(_program: &str, _opts: &Options) -> Result<(), git2::Error> {
     //let brief = format!("Usage: {} FILE [options]", _program);
-    //print!("hash_list_commit_message:\n{}", _opts.usage(&brief));
+    //print!("ref_hash_list_commit_message:\n{}", _opts.usage(&brief));
     let repo = match Repository::open(".") {
         Ok(repo) => repo,
         Err(e) => panic!("Error opening repository: {}", e),
@@ -68,42 +49,40 @@ pub fn hash_list_w_commit_message(_program: &str, _opts: &Options) -> Result<(),
     }
     Ok(())
     // process::exit(0);
-}
+} //end ref_hash_list_w_commit_message
+mod std_input {
 
-fn do_work(inp: &str, out: Option<String>) {
-    println!("{}", inp);
-    match out {
-        Some(x) => println!("{}", x),
-        None => println!("No Output"),
-    }
-}
+    extern crate getopts;
+    use crate::ref_hash_list;
+    use ascii::AsciiChar;
+    use getopts::Options;
+    use std::env;
+    use std::io;
+    use std::process;
 
-#[allow(dead_code)]
-fn print_type_of<T>(_: &T) -> String {
-    format!("{}", std::any::type_name::<T>())
-}
+    pub fn parse_input() {
+        let args: Vec<String> = env::args().collect();
+        let program = args[0].clone();
 
-pub fn main() -> Result<(), git2::Error> {
-    // Collect and parse arguments
-    let args: Vec<String> = env::args().collect();
-    let program = args[0].clone();
+        let mut opts = Options::new();
+        opts.optopt("o", "output", "set output file name", "NAME");
+        opts.optopt(
+            "r",
+            "ref",
+            "Specify the Git reference (default: HEAD)",
+            "REF",
+        );
+        opts.optopt(
+            "n",
+            "number",
+            "Specify the maximum number of commits to show (default: 10)",
+            "NUMBER",
+        );
 
-    let mut opts = Options::new();
-    opts.optopt("o", "output", "set output file name", "NAME");
-    opts.optopt("r", "ref", "Specify the Git reference (default: HEAD)", "REF");
-    opts.optopt("n", "number", "Specify the maximum number of commits to show (default: 10)", "NUMBER");
+        opts.optopt("s", "sec", "use following privkey", "SEC");
 
-    opts.optopt("s", "sec", "use following privkey", "SEC");
-
-    opts.optflag("h", "help", "print this help menu");
-    opts.optflag("m", "messages", "print reflog with commit messages");
-
-    //println!("args.len()={}",args.len());
-
-    if args.len() == 1 {
-
-        //println!("program={}",program);
-        //println!("args[0]={}",args[0]);
+        opts.optflag("h", "help", "print this help menu");
+        opts.optflag("m", "messages", "print reflog with commit messages");
 
         let mut input = String::new();
         //capture input from prompt
@@ -117,54 +96,53 @@ pub fn main() -> Result<(), git2::Error> {
         //REF: https://docs.rs/ascii/latest/ascii/enum.AsciiChar.html#method.is_ascii_hexdigit
         for (_i, c) in input.trim().chars().enumerate() {
             if c.is_ascii() {
-            //TODO:more validation
-            assert!(ascii::AsciiChar::Space.is_ascii_blank());
-            assert!(ascii::AsciiChar::Space.is_ascii_blank());
-            assert!(ascii::AsciiChar::Tab.is_ascii_blank());
-            assert!(!ascii::AsciiChar::VT.is_ascii_blank());
-            assert!(!ascii::AsciiChar::LineFeed.is_ascii_blank());
-            assert!(!ascii::AsciiChar::CarriageReturn.is_ascii_blank());
-            assert!(!ascii::AsciiChar::FF.is_ascii_blank());
+                //TODO:more validation
+                assert!(ascii::AsciiChar::Space.is_ascii_blank());
+                assert!(ascii::AsciiChar::Space.is_ascii_blank());
+                assert!(ascii::AsciiChar::Tab.is_ascii_blank());
+                assert!(!ascii::AsciiChar::VT.is_ascii_blank());
+                assert!(!ascii::AsciiChar::LineFeed.is_ascii_blank());
+                assert!(!ascii::AsciiChar::CarriageReturn.is_ascii_blank());
+                assert!(!ascii::AsciiChar::FF.is_ascii_blank());
 
-            assert!(AsciiChar::Space.is_ascii_blank());
-            assert!(AsciiChar::Tab.is_ascii_blank());
-            assert!(!AsciiChar::VT.is_ascii_blank());
-            assert!(!AsciiChar::LineFeed.is_ascii_blank());
-            assert!(!AsciiChar::CarriageReturn.is_ascii_blank());
-            assert!(!AsciiChar::FF.is_ascii_blank());
+                assert!(AsciiChar::Space.is_ascii_blank());
+                assert!(AsciiChar::Tab.is_ascii_blank());
+                assert!(!AsciiChar::VT.is_ascii_blank());
+                assert!(!AsciiChar::LineFeed.is_ascii_blank());
+                assert!(!AsciiChar::CarriageReturn.is_ascii_blank());
+                assert!(!AsciiChar::FF.is_ascii_blank());
 
-            #[cfg(debug_assertions)]
-            if c.is_ascii_control() {
-                println!("{}.is_asscii_control()={}",c,c.is_ascii_control());
-            }
-            #[cfg(debug_assertions)]
-            if c.is_ascii() {
-                println!("{}.is_asscii()={}",c,c.is_ascii());
-            }
-            #[cfg(debug_assertions)]
-            if c.is_ascii_graphic() {
-                println!("{}.is_asscii_graphic()={}",c,c.is_ascii_graphic());
-            }
-            #[cfg(debug_assertions)]
-            if c.is_whitespace() {
-                println!("{}.is_whitespace()={}",c,c.is_whitespace());
-            }
-            #[cfg(debug_assertions)]
-            if c.is_ascii_whitespace() {
-                println!("{}.is_ascii_whitespace()={}",c,c.is_ascii_whitespace());
-            }
-            #[cfg(debug_assertions)]
-            if !c.is_ascii_hexdigit() {
-                key_maybe = false;
-            }
-            #[cfg(debug_assertions)]
-            println!("{}:{}={}", _i, c, c.is_ascii_hexdigit());
-            #[cfg(debug_assertions)]
-            println!("{}", c);
-            count = count + 1;
-            
-        }//end is_ascii
-        }//end for loop
+                #[cfg(debug_assertions)]
+                if c.is_ascii_control() {
+                    println!("{}.is_asscii_control()={}", c, c.is_ascii_control());
+                }
+                #[cfg(debug_assertions)]
+                if c.is_ascii() {
+                    println!("{}.is_asscii()={}", c, c.is_ascii());
+                }
+                #[cfg(debug_assertions)]
+                if c.is_ascii_graphic() {
+                    println!("{}.is_asscii_graphic()={}", c, c.is_ascii_graphic());
+                }
+                #[cfg(debug_assertions)]
+                if c.is_whitespace() {
+                    println!("{}.is_whitespace()={}", c, c.is_whitespace());
+                }
+                #[cfg(debug_assertions)]
+                if c.is_ascii_whitespace() {
+                    println!("{}.is_ascii_whitespace()={}", c, c.is_ascii_whitespace());
+                }
+                #[cfg(debug_assertions)]
+                if !c.is_ascii_hexdigit() {
+                    key_maybe = false;
+                }
+                #[cfg(debug_assertions)]
+                println!("{}:{}={}", _i, c, c.is_ascii_hexdigit());
+                #[cfg(debug_assertions)]
+                println!("{}", c);
+                count = count + 1;
+            } //end is_ascii
+        } //end for loop
 
         #[cfg(debug_assertions)]
         println!("count={:?}", count);
@@ -189,70 +167,78 @@ pub fn main() -> Result<(), git2::Error> {
             if input.trim() == "PRIVKEY" {
                 println!("PRIVKEY={}", input);
             }
-            let _ = hash_list(&program, &opts);
+            let _ = ref_hash_list(&program, &opts);
             process::exit(0);
-        }//end else
-    //end if args.len() == 1
-    } else {
-        //let _ = hash_list(&program, &opts);
-        //print_usage(&program, &opts);
-        //process::exit(0);
-    }
-    if args.len() > 1 {
+        } //end else
+    } //end if args.len() == 1
+} //end parse_input
 
-    let matches = match opts.parse(&args[1..]) {
-        Ok(m) => m,
-        Err(f) => {
-            panic!("{}", f.to_string())
+pub fn print_usage(program: &str, opts: &Options) {
+    let brief = format!("Usage: {} [options]", program);
+    print!("{}", opts.usage(&brief));
+    process::exit(0);
+}
+pub fn sec(program: &str, opts: &Options) {
+    let brief = format!("Usage: {} [options]", program);
+    print!("sec:\n{}", opts.usage(&brief));
+    process::exit(0);
+}
+pub fn hash(program: &str, opts: &Options) {
+    //hash a following value
+    //TODO detect stream or file
+    let brief = format!("Usage: {} [options]", program);
+    print!("hash:\n{}", opts.usage(&brief));
+    process::exit(0);
+}
+
+pub fn main() -> Result<(), git2::Error> {
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+    let mut opts = Options::new();
+    opts.optopt("o", "output", "set output file name", "NAME");
+    opts.optopt(
+        "r",
+        "ref",
+        "Specify the Git reference (default: HEAD)",
+        "REF",
+    );
+    opts.optopt(
+        "n",
+        "number",
+        "Specify the maximum number of commits to show (default: 10)",
+        "NUMBER",
+    );
+
+    opts.optopt("s", "sec", "use following privkey", "SEC");
+
+    opts.optflag("h", "help", "print this help menu");
+    opts.optflag("m", "messages", "print reflog with commit messages");
+
+    if args.len() > 1 {
+        let matches = match opts.parse(&args[1..]) {
+            Ok(m) => m,
+            Err(f) => {
+                panic!("{}", f.to_string())
+            }
+        };
+
+        if matches.opt_present("h") {
+            print_usage(&program, &opts);
+            process::exit(0);
+        }
+        if matches.opt_present("s") {
+            sec(&program, &opts);
+            process::exit(0);
+        }
+        if matches.opt_present("m") {
+            let _ = ref_hash_list_w_commit_message(&program, &opts);
+        } else {
+            let _ = ref_hash_list_w_commit_message(&program, &opts);
+            let _ = ref_hash_list(&program, &opts);
         }
     };
 
-    //let output = matches.opt_str("o");
-    ////let output = if matches.opt_present("o") {
-    ////    //println!("output:0{:}", &args[0].to_string());
-    ////    //println!("output:1{:}", &args[1].to_string());
-    ////    //println!("output:2{:}", &args[2].to_string());
-    ////    //println!("output:3{:}", &args[3].to_string());
-    ////    //print_usage(&program, &opts);
-    ////    //process::exit(0);
-    ////} else {
-    ////    print_usage(&program, &opts);
-    ////};
-    //let input = if !matches.free.is_empty() {
-    //    //catch a free floating arg before or after -o <String>
-    //    matches.free[0].clone().to_string();
-    //    //println!("input:0{:}", &args[0].to_string());
-    //    //println!("input:1{:}", &args[1].to_string());
-    //    //println!("input:2{:}", &args[2].to_string());
-    //    //println!("input:3{:}", &args[3].to_string());
-    //} else {
-    //    print_usage(&program, &opts);
-    //};
-
-    //println!("!====={} true", !matches.free.is_empty());
-    //println!("====={} false", matches.free.is_empty());
-    //println!("====={}", matches.free[0].clone().to_string());
-    ////println!("====={}", matches.free[1].clone().to_string());
-    //println!("input===={:?}", input);
-    //println!("output==={:?}", output);
-    ////do_work(&input, output);
-    //process::exit(0);
-
-    if matches.opt_present("h") {
-        print_usage(&program, &opts);
-        process::exit(0);
-    }
-    if matches.opt_present("s") {
-        sec(&program, &opts);
-        process::exit(0);
-    }
-    if matches.opt_present("m") {
-        let _ = hash_list_w_commit_message(&program, &opts);
-    } else {
-        let _ = hash_list_w_commit_message(&program, &opts);
-        //let _ = hash_list(&program, &opts);
-    }
-    }
+    std_input::parse_input();
 
     Ok(())
 }
