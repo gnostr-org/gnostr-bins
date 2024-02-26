c2d421684b87c507a9034576260d71a5d750e715
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-02-25 19:29:06 -0500

    src/bin/gnostr-blockheight.rs:clean up

--- src/bin/gnostr-blockheight.rs
+++ src/bin/gnostr-blockheight.rs
@@ -1,18 +1,5 @@
-use reqwest::Url;
-use std::io::Read;
 use gnostr_bins::{get_blockheight};
 
-//fn mp_reqwest(){
-//
-//    let url = Url::parse("https://mempool.space/api/blocks/tip/height").unwrap();
-//    let mut res = reqwest::blocking::get(url).unwrap();
-//
-//    let mut body = String::new();
-//    res.read_to_string(&mut body).unwrap();
-//
-//    //println!("{}", body);
-//
-//}
 fn main() {
 
   let bh = get_blockheight();
