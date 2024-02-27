53ce10fb2bc01bf45032260bdaac13e91f11bae7
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-02-27 15:05:22 -0500

    src/bin/gnostr-wobble.rs:clean report

--- src/bin/gnostr-wobble.rs
+++ src/bin/gnostr-wobble.rs
@@ -1,26 +1,36 @@
 use reqwest::Url;
 use std::io::Read;
 use gnostr_bins::{get_blockheight};
-use gnostr_bins::{get_weeble};
-use gnostr_bins::{get_wobble};
+
+use std::time::{SystemTime, UNIX_EPOCH};
+
 fn main() {
 
-  let get_bh = get_blockheight();
-  //let mut bh_body = String::new();
-  let mut bh_body = get_bh.clone();
-  println!("{}", bh_body.unwrap().to_string());
+  let start = SystemTime::now();
+  let now = start
+    .duration_since(UNIX_EPOCH)
+    .expect("Time went backwards");
 
 
-  //let get_weeble = get_weeble();
-  //let get_wobble = get_wobble();
+  let since_the_epoch =
+    SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).expect("get millis error");
+    let seconds = since_the_epoch.as_secs();
+    let subsec_millis = since_the_epoch.subsec_millis() as u64;
+    let now_millis = seconds * 1000 + subsec_millis;
+    //println!("now millis: {}", seconds * 1000 + subsec_millis);
 
-  //let url = Url::parse("https://mempool.space/api/blocks/tip/height").unwrap();
-  //let mut res = reqwest::blocking::get(url).unwrap();
+  //let mut tmp_stringi = String::new();
+  //now.read_to_string(&mut tmp_string).unwrap();
+  //let tmp_u32 = tmp_string.parse::<u32>().unwrap_or(0);
 
-  //let mut bh_body = String::new();
-  //res.read_to_string(&mut bh_body).unwrap();
-  //println!("{}", bh_body);
+  let _ = get_blockheight();
+    let url = Url::parse("https://mempool.space/api/blocks/tip/height").unwrap();
+    let mut res = reqwest::blocking::get(url).unwrap();
 
-  //let mut weeblebody = String::new();
-  //println!("{}", bh_body);
+    let mut tmp_string = String::new();
+    res.read_to_string(&mut tmp_string).unwrap();
+    let tmp_u64 = tmp_string.parse::<u64>().unwrap_or(0);
+
+    let wobble = now_millis as f64 % tmp_u64 as f64;
+    println!("{}", format!("{}", wobble.floor()));
 }
