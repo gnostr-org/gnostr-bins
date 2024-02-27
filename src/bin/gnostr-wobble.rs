9c5824c1573c8dfe5f3570342f4d38010cd83da7
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-02-27 15:09:52 -0500

    src:clean up
    src/bin/gnostr-weeble.rs
    src/bin/gnostr-wobble.rs

--- src/bin/gnostr-wobble.rs
+++ src/bin/gnostr-wobble.rs
@@ -6,22 +6,12 @@ use std::time::{SystemTime, UNIX_EPOCH};
 
 fn main() {
 
-  let start = SystemTime::now();
-  let now = start
-    .duration_since(UNIX_EPOCH)
-    .expect("Time went backwards");
-
-
   let since_the_epoch =
     SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).expect("get millis error");
     let seconds = since_the_epoch.as_secs();
     let subsec_millis = since_the_epoch.subsec_millis() as u64;
     let now_millis = seconds * 1000 + subsec_millis;
     //println!("now millis: {}", seconds * 1000 + subsec_millis);
-
-  //let mut tmp_stringi = String::new();
-  //now.read_to_string(&mut tmp_string).unwrap();
-  //let tmp_u32 = tmp_string.parse::<u32>().unwrap_or(0);
 
   let _ = get_blockheight();
     let url = Url::parse("https://mempool.space/api/blocks/tip/height").unwrap();
