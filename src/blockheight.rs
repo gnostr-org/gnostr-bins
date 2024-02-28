569baaae9f6523dc5dd0bc356710d9570f08c4e8
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-02-28 13:52:03 -0500

    src/blockheight.rs:return f64

--- src/blockheight.rs
+++ src/blockheight.rs
@@ -1,13 +1,34 @@
-pub fn blockheight() -> Result<String, &'static str> {
-
+use std::time::SystemTime;
 use reqwest::Url;
 use std::io::Read;
+use crate::get_blockheight;
 
+pub fn check_curl() {
+
+    //println!("check_curl");
+}
+
+pub fn blockheight() -> Result<f64,ascii::AsciiChar> {
+
+
+  //let since_the_epoch =
+    //SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).expect("get millis error");
+    //let seconds = since_the_epoch.as_secs();
+    //let subsec_millis = since_the_epoch.subsec_millis() as u64;
+    //let now_millis = seconds * 1000 + subsec_millis;
+    //println!("now millis: {}", seconds * 1000 + subsec_millis);
+
+  let _ = get_blockheight();
     let url = Url::parse("https://mempool.space/api/blocks/tip/height").unwrap();
     let mut res = reqwest::blocking::get(url).unwrap();
 
-    let mut body = String::new();
-    res.read_to_string(&mut body).unwrap();
-    Ok(format!("{}", body))
+    let mut tmp_string = String::new();
+    res.read_to_string(&mut tmp_string).unwrap();
+    let tmp_u64 = tmp_string.parse::<u64>().unwrap_or(0);
+
+
+    //let weeble = now_millis as f64 / tmp_u64 as f64;
+    let blockheight =                  tmp_u64 as f64;
+    return Ok(blockheight.floor());
 
 }
