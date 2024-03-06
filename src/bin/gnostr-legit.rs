000002287e9757768342a89bc95448665c852af7
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-03-06 16:10:05 -0500

    2051/833449/355518:gnostr-legit
    "tree":"7ec6e769ab97d6f06102ed02f229af763d586c6d","parent":"000004100d1e9e604395a5a84bca7b4aa47e5524","weeble":"2051","blockheight":"833449","wobble":"355518","bit":"01","nonce":"0008056e","message":"gnostr-legit"

--- src/bin/gnostr-legit.rs
+++ src/bin/gnostr-legit.rs
@@ -359,7 +359,6 @@ fn main() -> io::Result<()> {
             .args([
                 "--sec",
                 "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f",
-                "$(gnostr-sha256 $(gnostr-weeble || echo))",
                 "-t",
                 "gnostr",
                 //"--tag",
