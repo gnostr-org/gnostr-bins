00000793586a7cf34957ea5ccd121fc12ddeb9a8
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-03-06 15:53:08 -0500

    2051/833448/356553:gnostr-legit
    "tree":"b144f33b173dd96012a915cd939f4120ba044ab1","parent":"000007c55368270ae2a0e9779ba12abe692d8ab6","weeble":"2051","blockheight":"833448","wobble":"356553","bit":"02","nonce":"000072d7","message":"gnostr-legit"

--- src/bin/gnostr-legit.rs
+++ src/bin/gnostr-legit.rs
@@ -360,6 +360,7 @@ fn main() -> io::Result<()> {
                 "-c",
                 "gnostr",
                 "--sec",
+                "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f",
                 "$(gnostr-sha256 $(gnostr-weeble || echo))",
                 //"-t",
                 //"gnostr",
@@ -373,7 +374,7 @@ fn main() -> io::Result<()> {
                 //"blockheight",
                 //"$(gnostr-blockheight || echo blockheight)",
                 "--content",
-                "\"$(gnostr-git show HEAD)\" ",
+                "\"$(git show HEAD)\" ",
             ])
             .output()
             .expect("failed to execute process")
