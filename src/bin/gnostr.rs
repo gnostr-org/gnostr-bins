f61fc380dd47e2e7007ee2ff9c790b99ae5b23ec
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-03-08 19:30:21 -0500

    src/bin/gnostr.rs:intermediate:
    parsing all args

--- src/bin/gnostr.rs
+++ src/bin/gnostr.rs
@@ -1,8 +1,10 @@
 use enum_iterator::{all, Sequence};
 use std::num::ParseFloatError;
+use std::path::PathBuf;
+use std::process;
 use structopt::StructOpt;
 
-#[derive(StructOpt)]
+#[derive(StructOpt, Debug)]
 struct Point {
     x: f64,
     y: f64,
@@ -14,7 +16,7 @@ impl Point {
     }
 }
 
-#[derive(StructOpt)]
+#[derive(StructOpt, Debug)]
 //sec_command
 struct SecCommand {
     //#[structopt(name = "sec", short = "s")]
@@ -26,7 +28,7 @@ struct SecCommand {
     //point: Point,
 }
 
-#[derive(StructOpt)]
+#[derive(StructOpt, Debug)]
 //add_command
 struct AddCommand {
     #[structopt(name = "operand1", short = "x")]
@@ -35,14 +37,14 @@ struct AddCommand {
     operand2: i32,
 }
 
-#[derive(StructOpt)]
+#[derive(StructOpt, Debug)]
 //sub_command
 struct SubCommand {
     //#[structopt(name = "value", short = "v")]
     value: i32,
 }
 
-#[derive(StructOpt)]
+#[derive(StructOpt, Debug)]
 struct Circle {
     #[structopt(name = "operand1", short = "x")]
     x: f64,
@@ -64,19 +66,36 @@ impl Circle {
     }
 }
 
-#[derive(StructOpt)]
+#[derive(StructOpt, Debug)]
 enum Shape {
     //gnostr shape circle help   - displays structopt derived help
     //gnostr shape circle --help - displays help = "FORMATTED HELP"
-    #[structopt(name = "circle", about = "Circle", help = "71:\n\tUSAGE:\n\t\tHELP1\n\t\tHELP2")]
+    #[structopt(
+        name = "shape____",
+        about = "shape_______",
+        help = "71:\n\tUSAGE:\n\t\tHELP1\n\t\t\tHELP2"
+    )]
+    #[structopt(
+        name = "circle",
+        about = "circle",
+        help = "72:\n\tUSAGE:\n\t\tHELP1\n\t\t\tHELP2"
+    )]
     Circle,
-    #[structopt(name = "square", about = "Square")]
+    #[structopt(
+        name = "square",
+        about = "square",
+        help = "73:\n\tUSAGE:\n\t\tHELP1\n\t\t\tHELP2"
+    )]
     Square,
-    #[structopt(name = "triangle", about = "Triangle")]
+    #[structopt(
+        name = "triangle",
+        about = "triangle",
+        help = "75:\n\tUSAGE:\n\t\tHELP1\n\t\t\tHELP2"
+    )]
     Triangle,
 }
 
-#[derive(StructOpt)]
+#[derive(StructOpt, Debug)]
 enum TrafficLight {
     #[structopt(name = "light")]
     #[structopt(name = "Red", about = "Red")]
@@ -87,16 +106,16 @@ enum TrafficLight {
     Green,
 }
 
-#[derive(StructOpt)]
-#[structopt(name = "gnostr", about = "gnostr: a git+nostr command line utility")]//, help = "89:HELP")]
+#[derive(StructOpt, Debug)]
+#[structopt(name = "gnostr", about = "gnostr: a git+nostr command line utility")] //, help = "89:HELP")]
 enum Cli {
-    #[structopt(name = "--sec", about = "<private_key>")]//, help = "91:HELP")]
+    #[structopt(name = "--sec", about = "<private_key>")] //, help = "91:HELP")]
     Sec(SecCommand),
-    #[structopt(name = "add", about = "-x <int> -y <int>")]//, help = "93:HELP")]
+    #[structopt(name = "add", about = "-x <int> -y <int>")] //, help = "93:HELP")]
     Add(AddCommand),
-    #[structopt(name = "sub", about = "-v <value>")]//, help = "95:HELP")]
+    #[structopt(name = "sub", about = "-v <value>")] //, help = "95:HELP")]
     Sub(SubCommand),
-    #[structopt(name = "shape", about = "TODO")]//, help = "97:HELP")]
+    #[structopt(name = "shape", about = "TODO")] //, help = "100:HELP")]
     Shape(Shape),
 }
 
@@ -104,102 +123,167 @@ fn detected_sec(private_key: String) {
     println!("63:private_key={}", private_key);
 }
 
+/// A basic example
+#[derive(StructOpt, Debug)]
+#[structopt(name = "basic")]
+struct Opt {
+    // A flag, true if used in the command line. Note doc comment will
+    // be used for the help message of the flag. The name of the
+    // argument will be, by default, based on the name of the field.
+    /// Activate debug mode
+    #[structopt(short, long)]
+    debug: bool,
+
+    // The number of occurrences of the `v/verbose` flag
+    /// Verbose mode (-v, -vv, -vvv, etc.)
+    #[structopt(short, long, parse(from_occurrences))]
+    verbose: u8,
+
+    // The number of occurrences of the `v/verbose` flag
+    // Verbose mode (-t, -tag, -tags, etc.)
+    #[structopt(short, long, parse(from_occurrences))]
+    tag: u8,
+
+    /// Set speed
+    #[structopt(short, long, default_value = "42")]
+    speed: f64,
+
+    /// Output file
+    #[structopt(short, long, parse(from_os_str))]
+    output: PathBuf,
+
+    // the long option will be translated by default to kebab case,
+    // i.e. `--nb-cars`.
+    /// Number of cars
+    #[structopt(short = "c", long)]
+    nb_cars: Option<i32>,
+
+    /// admin_level to consider
+    #[structopt(short, long)]
+    level: Vec<String>,
+
+    /// Files to process
+    #[structopt(name = "FILE", parse(from_os_str))]
+    files: Vec<PathBuf>,
+}
+
 fn main() {
     let private_key = {};
 
-    let string_slice = "3.14159";
+    let opt = Opt::from_args();
+    println!("{:#?}]", opt);
 
-    // Option 1: Using Result with pattern matching
-    match string_slice.parse::<f64>() {
-        Ok(value) => println!("Parsed value: {}", value),
-        Err(error) => println!("Error parsing string: {}", error),
+    let mut l_count = 0;
+    println!("opt.level={:#?}]", opt.level);
+    for l in opt.level {
+        println!("{}={:?}", l_count, l);
+        l_count = l_count + 1;
     }
 
-    // Option 2: Using `expect` (only for trusted input)
-    let parsed_value: f64 = string_slice.parse().expect("Failed to parse string");
-    println!("Parsed value: {}", parsed_value);
+    let mut f_count = 0;
+    println!("opt.files={:#?}]", opt.files);
+    for f in opt.files {
+        println!("{}={:?}", f_count, f);
+        f_count = f_count + 1;
+    }
+    process::exit(0);
 
-    let p = Point { x: 3.0, y: 4.0 };
-    let distance = p.distance_to_origin(); // Method call using dot notation
-    println!("Distance to origin: {}", distance);
-
-
-
-
-    let mut c = Circle {
-        x: 10.0,
-        y: 4.0,
-        r: 1.0,
-    };//a "contructor"
-
-    let distance_x = c.set_x(); // Method call using dot notation
-    println!("123:distance_x = c.set_x()={}", distance_x);
-    let distance_y = c.set_y(); // Method call using dot notation
-    println!("129:distance_y = c.set_y()={}", distance_y);
-    let distance_r = c.set_r(); // Method call using dot notation
-    println!("131:distance_r = c.set_r()={}", distance_r);
-
-    //let light = TrafficLight::Yellow;
-    //if let TrafficLight::Red = light {
-    //    println!("Stop!");
-    //} else if let TrafficLight::Yellow = light {
-    //    println!("Caution!");
-    //} else {
-    //    println!("Go!");
-    //}
-
-    //let shape = Shape::Circle;
-    //if let Shape::Circle = shape {
-    //    println!("75:circle!");
-    //} else if let Shape::Square = shape {
-    //    println!("77:square");
-    //} else if let Shape::Triangle = shape {
-    //    println!("79:must be triangle");
-    //} else {}
-
-    //let gnostr_shape = Cli::from_args();
-    ////match test { Cli::Shape(..) => {
-    //match gnostr_shape {
-    //    Cli::Shape(shape) => {
-    //        let result = shape;
-    //        //println!("test: {}", result);
+    //    let string_slice = "3.14159";
+    //
+    //    // Option 1: Using Result with pattern matching
+    //    match string_slice.parse::<f64>() {
+    //        Ok(value) => println!("Parsed value: {}", value),
+    //        Err(error) => println!("Error parsing string: {}", error),
     //    }
-    //    Cli::Sec(_) | Cli::Add(_) | Cli::Sub(_) => todo!(),
-    //}
-
-    let matches = Cli::from_args();
-    let result = "";
-    match matches {
-        Cli::Sec(sec_command) => {
-            let private_key = sec_command.private_key;
-            detected_sec(private_key.clone());
-            println!("102:--sec={}", private_key);
-        }
-        Cli::Add(add_command) => {
-            let result = add_command.operand1 + add_command.operand2;
-            println!("Sum: {}", result);
-            println!("102:--sec={:?}", private_key);
-        }
-        Cli::Sub(sub_command) => {
-            let result = sub_command.value;
-            println!("Subcommand value: {}", sub_command.value);
-            println!("106:--sec={:?}", private_key);
-        }
-        Cli::Shape(shape) => {
-            let result = shape;
-            // println!("test: {:?}", result);
-            println!("117:--sec={:?}", private_key);
-        } //println!("result={}", result);
-    }
-
-    let p = Point { x: 12.0, y: 4.0 };
-    let distance = p.distance_to_origin(); // Method call using dot notation
-    println!("Distance to origin: {}", distance);
-    let mut c = Circle {
-        x: 10.0,
-        y: 4.0,
-        r: 1.0,
-    };
-    let distance = c.set_x(); // Method call using dot notation
-    println!("110:distance = c.set_x()={}", distance);
+    //
+    //    // Option 2: Using `expect` (only for trusted input)
+    //    let parsed_value: f64 = string_slice.parse().expect("Failed to parse string");
+    //    println!("Parsed value: {}", parsed_value);
+    //
+    //    let p = Point { x: 3.0, y: 4.0 };
+    //    let distance = p.distance_to_origin(); // Method call using dot notation
+    //    println!("Distance to origin: {}", distance);
+    //
+    //
+    //
+    //
+    //    let mut c = Circle {
+    //        x: 10.0,
+    //        y: 4.0,
+    //        r: 1.0,
+    //    };//a "contructor"
+    //
+    //    let distance_x = c.set_x(); // Method call using dot notation
+    //    println!("123:distance_x = c.set_x()={}", distance_x);
+    //    let distance_y = c.set_y(); // Method call using dot notation
+    //    println!("129:distance_y = c.set_y()={}", distance_y);
+    //    let distance_r = c.set_r(); // Method call using dot notation
+    //    println!("131:distance_r = c.set_r()={}", distance_r);
+    //
+    //    //let light = TrafficLight::Yellow;
+    //    //if let TrafficLight::Red = light {
+    //    //    println!("Stop!");
+    //    //} else if let TrafficLight::Yellow = light {
+    //    //    println!("Caution!");
+    //    //} else {
+    //    //    println!("Go!");
+    //    //}
+    //
+    //    //let shape = Shape::Circle;
+    //    //if let Shape::Circle = shape {
+    //    //    println!("75:circle!");
+    //    //} else if let Shape::Square = shape {
+    //    //    println!("77:square");
+    //    //} else if let Shape::Triangle = shape {
+    //    //    println!("79:must be triangle");
+    //    //} else {}
+    //
+    //    //let gnostr_shape = Cli::from_args();
+    //    ////match test { Cli::Shape(..) => {
+    //    //match gnostr_shape {
+    //    //    Cli::Shape(shape) => {
+    //    //        let result = shape;
+    //    //        //println!("test: {}", result);
+    //    //    }
+    //    //    Cli::Sec(_) | Cli::Add(_) | Cli::Sub(_) => todo!(),
+    //    //}
+    //
+    //
+    //    process:exit(0);
+    //    let matches = Cli::from_args();
+    //    println!("{:#?}", matches);
+    //    let result = "";
+    //    match matches {
+    //        Cli::Sec(sec_command) => {
+    //            let private_key = sec_command.private_key;
+    //            detected_sec(private_key.clone());
+    //            println!("102:--sec={}", private_key);
+    //        }
+    //        Cli::Add(add_command) => {
+    //            let result = add_command.operand1 + add_command.operand2;
+    //            println!("Sum: {}", result);
+    //            println!("102:--sec={:?}", private_key);
+    //        }
+    //        Cli::Sub(sub_command) => {
+    //            let result = sub_command.value;
+    //            println!("Subcommand value: {}", sub_command.value);
+    //            println!("106:--sec={:?}", private_key);
+    //        }
+    //        Cli::Shape(shape) => {
+    //            let result = shape;
+    //            // println!("test: {:?}", result);
+    //            println!("117:--sec={:?}", private_key);
+    //        } //println!("result={}", result);
+    //    }
+    //
+    //    let p = Point { x: 12.0, y: 4.0 };
+    //    let distance = p.distance_to_origin(); // Method call using dot notation
+    //    println!("Distance to origin: {}", distance);
+    //    let mut c = Circle {
+    //        x: 10.0,
+    //        y: 4.0,
+    //        r: 1.0,
+    //    };
+    //    let distance = c.set_x(); // Method call using dot notation
+    //    println!("110:distance = c.set_x()={}", distance);
 }
