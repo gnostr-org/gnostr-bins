319edf04d7a84904ab5a7e819d274a61ef69a535
randymcmillan <randy.lee.mcmillan@gmail.com>
2024-04-30 20:19:30 -0400

    src/bin/gnostr-fetch-watch-list.rs:intermediate:clean up

--- src/bin/gnostr-fetch-watch-list.rs
+++ src/bin/gnostr-fetch-watch-list.rs
@@ -1,5 +1,4 @@
 use futures::executor::block_on;
-//use reqwest::into_url::IntoUrlSealed;
 use url::Url;
 
 async fn parse_urls(urls_str: &str) -> Result<Vec<String>, url::ParseError> {
@@ -8,55 +7,34 @@ async fn parse_urls(urls_str: &str) -> Result<Vec<String>, url::ParseError> {
     let mut collected = Vec::new();
     let mut char_iter = urls_str.chars();
     for url_str in urls_str.chars() {
-        print!("___________________{}",url_str);
-
-        if char_iter.next() == Some('[') {
-            //println!("{}", url_str);
-            //let comma: String = ",".to_string();
-            //collected.push(comma.clone());
-            //let skipped: String = "".to_string();
-            //collected.push(skipped);
-        }
-
+        if char_iter.next() == Some('[') {}
         loop {
-            //match char_iter.next().ok_or(/* ... */)? {
             match char_iter.next() {
                 Some(']') => {
-                    //let skipped: String = "".to_string();
-                    //collected.push(skipped);
-                    //collected.push(part);
-                    //let comma: String = ",".to_string();
-                    //collected.push(comma);
                     return std::result::Result::Ok(collected);
                 }
                 Some(',') | Some(' ') => {
                     if !part.is_empty() {
                         collected.push(part.clone());
-                        print!("{:}\n",part.clone().to_string());
+                        print!("{:}\n", part.clone().to_string());
                         part = String::new();
                     }
                 }
                 x => part.push(x.expect("REASON")),
             }
         } //end loop
-
-        //print!("LINE:8:{}\n", url_str);
-        //println!("LINE:8:{:?}\n", urls);
-
-        //let parsed_url = Url::parse(&url_str)?;
-        //urls.push(parsed_url.to_string());
     }
     Ok(urls)
 }
 
 async fn print_relay_list() {
-    let relay_list: &str = gnostr_bins::get_relays_public().unwrap().as_str();
-    let vec_relay_list = parse_urls(&gnostr_bins::get_relays_public().unwrap().as_str()).await;
-    //let vec_relay_list = parse_urls(gnostr_bins::get_relays_public().unwrap());
-    //    print!("{:}", format!("{}",vec_relay_list.unwrap()));
-    //println!("{:?}", vec_relay_list.unwrap());
-    //print!("{:#}", gnostr_bins::get_relays_public().unwrap());
-    print!("{:}", gnostr_bins::get_relays_public().unwrap());
+    //let mut vic = vec![String::from("Element 1"), String::from("Element 2")];
+    //for element in vic.iter_mut() {
+    //    element.push_str(" - modified"); // Example modification within the loop
+    //    println!("The modified element is: {}", element);
+    //}
+    //let relay_list: &str = gnostr_bins::get_relays_public().unwrap().as_str();
+    let mut vec_relay_list = parse_urls(&gnostr_bins::get_relays_public().unwrap().as_str()).await;
 }
 fn main() {
     let future = print_relay_list(); // Nothing is printed
