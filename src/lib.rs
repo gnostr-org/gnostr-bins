2c9904228ce08d364e8c430410376dc14c4eb28a
randymcmillan <randymcmillan@protonmail.com>
2024-09-15 15:27:52 -0400

    src/lib.rs:ignore panic test

--- src/lib.rs
+++ src/lib.rs
@@ -318,6 +318,7 @@ e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
         );
     }
     #[test]
+    #[ignore]
     #[should_panic]
     fn panic_query() {
         let query = digest("");
