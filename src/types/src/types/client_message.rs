62e90c73a8c0ba70f17dac7d8ea114bbe59b4117
randymcmillan <randymcmillan@protonmail.com>
2024-09-22 15:48:21 -0400

    src

--- src/types/src/types/client_message.rs
+++
@@ -1,4 +0,0 @@
-use crate::versioned::ClientMessageV3;
-
-/// A message from a client to a relay
-pub type ClientMessage = ClientMessageV3;
--- src/types/src/types/relay_message.rs
+++
@@ -1,4 +0,0 @@
-use crate::versioned::RelayMessageV5;
-
-/// A message from a relay to a client
-pub type RelayMessage = RelayMessageV5;
