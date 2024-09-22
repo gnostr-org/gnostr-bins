61e86d7d5e56e8e191c2d1df694862e41d143b56
randymcmillan <randymcmillan@protonmail.com>
2024-09-22 14:11:12 -0400

    brew/apt/apk add/install openssl

--- build.rs
+++ build.rs
@@ -2,4 +2,7 @@ use std::process::Command;
 
 fn main() {
     Command::new("cargo").args(["update", "nostr-types"]);
+    Command::new("brew").args(["install", "openssl@1.1", "||", "true"]);
+    Command::new("apt").args(["install", "openssl", "||", "true"]);
+    Command::new("apk").args(["add", "openssl-dev", "||", "true"]);
 }
